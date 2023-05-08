7bb969d1872da850f9c790988b5ca56c0a4deb05
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-05-08 22:47:39 +1000

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { NinetyRingWithBg } from "react-svg-spinners"
 
 enum CardGenerationState { idle, generating, generated }
 const classNames = (...classes: string[]) => {
@@ -9,27 +10,36 @@ const App = () => {
   return (
     <>
       <div className="bg-casino h-full p-4">
-        <div className="text-center p-4 text-xl w-3/4 mx-auto flex flex-col gap-4 backdrop-blur-xl rounded-xl border-gray-300/20 border h-full bg-black/50 text-gray-300">
+        <div className="text-center p-4 text-xl w-3/4 mx-auto flex flex-col gap-4 backdrop-blur-xl rounded-xl border-gray-300/20 border bg-black/50 text-gray-300">
           <h1 className="font-semibold text-4xl">
             Random Card Generator
           </h1>
           <p>
-            This card generator will randomly pick any card from a standard 54 card deck, with four suits: <b className="font-semibold">spades, hearts, diamonds, and clubs</b>. Each suit has 13 cards: <b className="font-semibold">Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, and King.</b> There are also two Jokers: <b className="font-semibold">red and black</b>.
+            This card generator will randomly pick any card from a standard 54 card deck, with four suits: <b className="font-semibold">spades, hearts, diamonds, and clubs</b>. Each suit has 13 cards: <b className="font-semibold">Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, and King.</b> Additionally, there are two Jokers: <b className="font-semibold">red and black</b>.
           </p>
           <button
-            className="border p-4 px-8 rounded-xl mx-auto border-gray-300/20 bg-white/10 shadow-2xl shadow-black/60 hover:bg-gray-300/70 transition hover:text-gray-700 hover:scale-110 font-semibold ease-in-out"
+            className="border p-4 px-8 rounded-xl mx-auto border-gray-300/20 bg-white/10 shadow-2xl shadow-black/60 hover:bg-gray-300/70 transition hover:text-gray-700 hover:scale-110 font-medium ease-in-out"
             onClick={() => {
-              setTimeout(() => { setCardGenerationState(cardGenerationState) }, 5000)
+              setTimeout(() => { setCardGenerationState(CardGenerationState.generated) }, Math.random() * 2000 + 2000)
+              setCardGenerationState(CardGenerationState.generating)
             }}
           >
-            Generate Card
+            {
+              cardGenerationState === CardGenerationState.generating
+                ?
+                <span className="flex gap-2 items-center">
+                  <NinetyRingWithBg color="white" />
+                  Generating Card...
+                </span>
+                : 'Generate Card'
+            }
           </button>
           <img
             src="card.png"
             alt="Card"
             className={classNames(
-              "w-1/3 mx-auto bg-black/50 rounded-xl border-white/10 p-2 border mt-8 transition ease-in-out duration-700",
-              cardGenerationState ? "opacity-100" : "opacity-0"
+              "w-1/3 mx-auto bg-white/70 rounded-xl border-gray-300/20 p-2 border my-8 transition ease-in-out duration-700",
+              cardGenerationState === CardGenerationState.generated ? "opacity-100" : "opacity-0"
             )}
           />
         </div>
